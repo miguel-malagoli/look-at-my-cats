f2aef199d0230823623771a44cf83be32a1a4fd1
Miguel Malagoli <miguel.g.malagoli@gmail.com>
2021-01-21 19:43:26 -0300

    Edited and imported photos

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import test from './images/bonnie/bonnie8-01.jpg';
 
 function App() {
   return (
     <div className="App">
       Look At My Cats
+      <img
+        src={test}
+      />
     </div>
   );
 }
