8792613804b316d0980da657bc06c4d4ae0c70bc
Miguel Malagoli <miguel.g.malagoli@gmail.com>
2021-01-25 11:52:24 -0300

    Apply desktop layout to Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -18,28 +18,40 @@ const Home = () => {
                 />
             </div>
             <h1 className="home__prompt">
-                Which <span className="home__prompt_highlight">cat</span> will
-                you <span className="home__prompt_highlight">look at?</span>
+                Which <span className="home__highlight">cat</span> will
+                you <span className="home__highlight">look at?</span>
             </h1>
+            <div className="home__feedback">
+                {CATS.map((cat: Cat, index: number) => { return (
+                    <p className="home__name" key={cat.name}>
+                    {cat.name.substr(0, 1)}
+                    <span className="home__highlight">
+                        {cat.name.substr(1, 1)}
+                    </span>
+                    {cat.name.substr(2)}
+                </p>
+                );})}
+            </div>
             <ul className="home__list">
                 {CATS.map((cat: Cat, index: number) => { return (
-                    <li key={cat.name} className="cat">
-                        <button className="cat__item">
+                    <li key={cat.name} className="home__item">
+                        <button className="home__button">
                             <img
-                                className="cat__photo"
+                                className="home__photo"
                                 src={cat.profileImg1}
                                 alt={cat.name}
                             />
-                            <div className="cat__fade">
-                                <p className="cat__name">
-                                    {cat.name.substr(0, 1)}
-                                    <span className="cat__letter">
-                                        {cat.name.substr(1, 1)}
-                                    </span>
-                                    {cat.name.substr(2)}
-                                </p>
-                            </div>
+                            
                         </button>
+                        <div className="home__fade">
+                            <p className="home__name">
+                                {cat.name.substr(0, 1)}
+                                <span className="home__highlight">
+                                    {cat.name.substr(1, 1)}
+                                </span>
+                                {cat.name.substr(2)}
+                            </p>
+                        </div>
                     </li>
                 );})}
             </ul>
