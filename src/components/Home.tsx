b332c1c441c179d7223f8b8f11c6ca1d65705f48
Miguel Malagoli <miguel.g.malagoli@gmail.com>
2021-01-25 13:08:50 -0300

    Add feedback to Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,5 @@
 // Import React
-import React from 'react';
+import React, { useState } from 'react';
 // Import constants
 import CATS, { Cat } from '../constants/cats/cats';
 // Import components
@@ -7,6 +7,9 @@ import Logo from './Logo';
 
 // Home component
 const Home = () => {
+
+    const [hoverItem, setHoverItem] = useState(-1);
+
     return (
         <main className="home">
             <div className="home__title">
@@ -17,13 +20,13 @@ const Home = () => {
                     alt="Look At My Cats"
                 />
             </div>
-            <h1 className="home__prompt">
+            <h1 className={'home__prompt' + ((hoverItem < 0 || hoverItem >= CATS.length) ? '' : ' home__prompt_hidden')}>
                 Which <span className="home__highlight">cat</span> will
                 you <span className="home__highlight">look at?</span>
             </h1>
             <div className="home__feedback">
                 {CATS.map((cat: Cat, index: number) => { return (
-                    <p className="home__name" key={cat.name}>
+                    <p className={'home__name' + (index === hoverItem ? ' home__name_visible' : '')} key={cat.name}>
                     {cat.name.substr(0, 1)}
                     <span className="home__highlight">
                         {cat.name.substr(1, 1)}
@@ -35,13 +38,17 @@ const Home = () => {
             <ul className="home__list">
                 {CATS.map((cat: Cat, index: number) => { return (
                     <li key={cat.name} className="home__item">
-                        <button className="home__button">
+                        <button
+                            className="home__button"
+                            onMouseEnter={() => {setHoverItem(index)}}
+                            onFocus={() => {setHoverItem(index)}}
+                            onMouseLeave={() => {setHoverItem(-1)}}
+                            onBlur={() => {setHoverItem(-1)}}>
                             <img
                                 className="home__photo"
                                 src={cat.profileImg1}
                                 alt={cat.name}
                             />
-                            
                         </button>
                         <div className="home__fade">
                             <p className="home__name">
